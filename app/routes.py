c0aded389bea20958bcc152f0cd4361e76a602b3
Jack Hynds <hyndsjt@pedarecc.sa.edu.au>
2019-10-31 13:20:24 +1030

    I dont know what this does

--- app/routes.py
+++ app/routes.py
@@ -23,10 +23,10 @@ def register():
 	if current_user.is_authenticated:
 		return redirect(url_for('home'))
 	form = RegistrationForm()
-	if form.validate_on_submit():
+	if form.validate_on_submit():		#If the form was valid
 		password_hash = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
-		user = User(username=form.username.data, email = form.email.data, password=password_hash)
-		db.session.add(user)
+		user = User(username=form.username.data, email = form.email.data, password=password_hash)	#Creates a variable user which holds the data for an entry to the database
+		db.session.add(user)			#Adds the user variable, which is set
 		db.session.commit()
 		flash(f'Account created for {form.username.data}!', 'success')
 		return redirect(url_for('login'))
@@ -34,13 +34,13 @@ def register():
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-	if current_user.is_authenticated:
+	if current_user.is_authenticated:	#If the user is already logged in, send them to the home page
 		return redirect(url_for('home'))
-	form = LoginForm()
-	if form.validate_on_submit():
-		user = User.query.filter_by(email = form.email.data).first()
-		if user and bcrypt.check_password_hash(user.password, form.password.data):
-			login_user(user, remember=form.remember.data)
+	form = LoginForm()		#Declares the variable form as the login form in forms.py
+	if form.validate_on_submit():	#If the form is valid
+		user = User.query.filter_by(email = form.email.data).first()	#Checks to see if the email matches one in the database
+		if user and bcrypt.check_password_hash(user.password, form.password.data):	#If there is an email that matches and the password hashes are for the same password
+			login_user(user, remember=form.remember.data)	#Logs in the user
 			next_page = request.args.get('next')
 			return redirect(next_page) if next_page else redirect(url_for('home'))
 		else:
